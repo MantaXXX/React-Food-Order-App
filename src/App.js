cce4c72959898f042cff531bd9d2d6f11f170268
Jia <pochialu@gmail.com>
2022-11-07 02:03:39 +0800

    新增 Cart Modal 遮罩效果子元件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
+
 import Header from "./components/Layout/Header";
 import Meal from "./components/Meals/Meal";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meal />
