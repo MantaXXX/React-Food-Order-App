43f11f9ca77295bcbfbc25e785f8ef6679690422
Jia <pochialu@gmail.com>
2022-11-08 02:18:52 +0800

    元件中使用context去處理新增邏輯, input 元件使用 forwardRef 取得 ref

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,12 @@
+import React from 'react'
 import classes from './Input.module.css'
 
 // {...props.input} 回傳 input 內所有的 key-pairs
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
   return <div className={classes.input}>
     <label htmlFor={classes.input.id}>{props.label}</label>
-    <input {...props.input} />
+    <input ref={ref} {...props.input} />
   </div>
-}
+})
 
 export default Input
