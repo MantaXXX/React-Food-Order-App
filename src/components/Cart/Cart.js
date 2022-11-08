6d48dbb13f39daea8002f01765254e99e0ac1fb5
Jia <pochialu@gmail.com>
2022-11-08 12:28:06 +0800

    cart 元件引用 cartItem 子元件，並取得 context 資料邏輯

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,32 @@
+import { useContext } from 'react'
 import Modal from '../UI/Modal'
+import CartItem from './CartItem'
 import classes from './Cart.module.css'
+import CartContext from '../../store/cart-context'
 
 const Cart = props => {
+  const cartCtx = useContext(CartContext)
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+  const hasItems = cartCtx.items.length > 0
+
+  const cartItemRemoveHandler = id => { }
+  const cartItemAddHandler = item => { }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[].map(item =>
-        <li>{item.name}</li>
-      )}
-    </ul>
+      {cartCtx.items.map(item =>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        > {item.name}</CartItem>
+      )
+      }
+    </ul >
   )
 
   return (
@@ -15,12 +34,12 @@ const Cart = props => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
+        {hasItems && <button className={classes.button}>Order</button>
+        } </div>
     </Modal>
   )
 }
